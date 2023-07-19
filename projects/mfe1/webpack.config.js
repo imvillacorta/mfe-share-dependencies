7c4b8e8adaa880e4b2936375b5f8dcec0f333302
Ivan Villacorta <ivan_mv23@hotmail.com>
2023-07-18 22:42:24 -0300

    setup initial

---
+++ projects/mfe1/webpack.config.js
@@ -0,0 +1,42 @@
+const { share, shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'mfe1',
+
+  exposes: {
+    './Module': './projects/mfe1/src/app/flights/flights.module.ts',
+  },
+
+  // shared: {
+  //   ...shareAll({
+  //     singleton: true,
+  //     strictVersion: true,
+  //     requiredVersion: 'auto'
+  //   }),
+  // },
+
+  shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }
+  }),
+
+});
